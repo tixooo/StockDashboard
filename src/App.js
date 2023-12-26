f56b375c27f1e725da879fb6fa265cafe607d50f
Stefanov <konstantin.stefanov@dxc.com>
2023-12-26 22:36:47 +0200

    added few minor changes to the login form and register form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
     SignIn,
     SignUp,
 } from './utils/barrelComponents.js';
-import React from "react";
+// import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 let App = () => {
