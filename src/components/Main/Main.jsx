a5393a24db6194104d34037fc61e880cc90188ab
Stefanov <konstantin.stefanov@dxc.com>
2023-12-27 11:38:18 +0200

    added some basic main panel texts for us to work with and do some basic tailwind on

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,13 @@ import React from 'react';
 export default function Main() {
     return (
         <>
-           <p>Work in progress</p>
+           <p>General information about the website</p>
+            <p>Why you should use the App</p>
+            <p>Number of registered users</p>
+            <p>Number of total transactions</p>
+            <p>Number of daily transactions</p>
+            <p>Special AI assistant (MAYBE!)</p>
+            <p>Other stuff...</p>
         </>
     )
 }
