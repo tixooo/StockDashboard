76e7e35b0595a4dee680f4df1dcdc8ffb0735451
Stefanov <konstantin.stefanov@dxc.com>
2023-12-28 00:59:11 +0200

    added basic setup for navbar without the logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,70 +28,65 @@ export default function Navbar() {
         show={showProfileModal}
         handleClose={handleCloseModal}
       />
-      <div className="container text-center">
-        <div className="row">
-          <div className="col">
-            <div className="bg-primary-subtle">
-              Logo to be implemented (auto generate something with AI)
-            </div>
-          </div>
-          <div className="col">
-            <div className="navigation-menu bg-info-subtle">
-              <NavLink to="/" className="wrapper-logo"></NavLink>
-              <ul className="list">
-                <li>
-                  <NavLink to="/" className="badge bg-primary">
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="info" className="badge bg-primary">
-                    Info
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="forum" className="badge bg-primary">
-                    Forum
-                  </NavLink>
-                </li>
-                {isAuthenticated ? (
-                  <>
-                    <li>
-                      <button
-                        onClick={handleProfileClick}
-                        className="badge bg-primary"
-                      >
-                        Profile
-                      </button>
-                    </li>
-                    <li>
-                      <button
-                        onClick={handleLogout}
-                        className="badge bg-primary"
-                      >
-                        Log Out
-                      </button>
-                    </li>
-                  </>
-                ) : (
-                  <>
-                    <li>
-                      <NavLink to="login" className="badge bg-primary">
-                        Log In
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink to="register" className="badge bg-primary">
-                        Register
-                      </NavLink>
-                    </li>
-                  </>
-                )}
-              </ul>
-            </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            Logo Here
+          </NavLink>
+          <div className="d-flex justify-content-end w-100">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/info">
+                  Info
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/forum">
+                  Forum
+                </NavLink>
+              </li>
+              {isAuthenticated ? (
+                <>
+                  <li className="nav-item">
+                    <button
+                      onClick={handleProfileClick}
+                      className="btn btn-link nav-link"
+                    >
+                      Profile
+                    </button>
+                  </li>
+                  <li className="nav-item">
+                    <button
+                      onClick={handleLogout}
+                      className="btn btn-link nav-link"
+                    >
+                      Log Out
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/login">
+                      Log In
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/register">
+                      Register
+                    </NavLink>
+                  </li>
+                </>
+              )}
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
