866f6f067d20d2b4e17a3a23291a2140311fe4e3
Stefanov <konstantin.stefanov@dxc.com>
2023-12-28 01:17:20 +0200

    fixed the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Footer.css';
 const Footer = () => {
   return (
-    <div className="border p-3 fixed-bottom bg-info">
-      <p className="footer text-center">&copy; Stock Market Dashboard</p>
-    </div>
+    <footer className="bg-light border-top p-3 fixed-bottom">
+      <div className="container d-flex justify-content-between">
+        <p className="mb-0">© Stock Market Dashboard</p>
+        <div>
+          <a href="https://www.facebook.com" className="social-icon">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.youtube.com" className="social-icon">
+            <i className="fab fa-youtube"></i>
+          </a>
+          <a href="https://www.twitter.com" className="social-icon">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://www.linkedin.com" className="social-icon">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
